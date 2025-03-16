60f276dfdbf061fcb58293eaac2dc02fc89bcf77
theodorebennett109 <theodorebennett3@gmail.com>
2025-03-16 04:22:16 -0500

    bus location api from aws

--- routes/web.php
+++ routes/web.php
@@ -2,18 +2,35 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+// Route::get('/', function () {
+//     return Inertia::render('Welcome', [
+//         'canLogin' => Route::has('login'),
+//         'canRegister' => Route::has('register'),
+//         'laravelVersion' => Application::VERSION,
+//         'phpVersion' => PHP_VERSION,
+//     ]);
+// });
+Route::get('/bus-location/{busId}', function ($busId) {
+    $apiUrl = "https://vq1kjn83jl.execute-api.us-east-1.amazonaws.com/prod/getLocationById/{$busId}";
+
+    $response = Http::get($apiUrl);
+
+    if ($response->successful()) {
+        return response()->json($response->json(), 200);
+    }
+
+    return response()->json(['error' => 'Bus location not found'], 404);
 });
 
+
+
+Route::get('/', function (){
+    return Inertia::render('Map');
+});
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
